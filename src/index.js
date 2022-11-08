710e5d87f162e0c61bd70c0b8a416f88ba48d552
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-11-08 23:06:15 +0530

    CREATE: login integrated

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://localhost:5000/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
