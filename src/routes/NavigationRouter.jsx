cda3b1d56bc497e3d013219bb1389abc2fcfbe14
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-11-08 20:31:17 +0530

    CREATE: login page created

--- src/routes/NavigationRouter.jsx
+++ src/routes/NavigationRouter.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 const NavigationRouter = () => {
   return (
     <Routes>
       <Route element={<Home />} path="/" />
+      <Route element={<Login />} path="login" />
     </Routes>
   );
 };
