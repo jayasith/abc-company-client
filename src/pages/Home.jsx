ce7ec030c10379dadec59a39f064edc65492f557
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-11-10 11:02:40 +0530

    CREATE: ui fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Header from '../components/Header';
+import CTAImage from './../assets/cta-img.png';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <section class="text-gray-600 body-font h-[calc(100vh-80px)] flex items-center">
+        <div class="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
+          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 class="sm:text-5xl text-3xl mb-4 font-bold text-gray-900 max-w-[750px]">
+              Upload Your Files and Messages in One Place
+            </h1>
+            <p class="mb-8 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius sunt dolor nesciunt consectetur perspiciatis
+              quia saepe delectus ab et beatae.
+            </p>
+            <div class="flex justify-center">
+              <Link to="/login">
+                <button className="inline-flex items-center bg-indigo-500 border-0 py-2 px-4 focus:outline-none text-white hover:bg-indigo-600 rounded text-base md:mt-0">
+                  Login
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-4 h-4 ml-1"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img class="object-cover object-center rounded transform -scale-x-100" alt="hero" src={CTAImage} />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- src/routes/NavigationRouter.jsx
+++ src/routes/NavigationRouter.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Login from '../pages/Login';
-import Register from "../pages/Register";
+import Register from '../pages/Register';
 import Worker from '../pages/Worker';
-import Manager from "../pages/Manager";
-import ManagerMessage from "../components/MangerMessage";
-import WorkerMessage from "../components/WorkerMessage";
+import Manager from '../pages/Manager';
+import ManagerMessage from '../components/MangerMessage';
+import WorkerMessage from '../components/WorkerMessage';
 import FileUploader from '../components/FileUploader';
-import AdminDashboard from "../pages/AdminDashboard";
-import RegisterUser from "../components/RegisterUser";
-import ViewUser from "../components/ViewUser";
+import AdminDashboard from '../pages/AdminDashboard';
+import ViewUser from '../components/ViewUser';
+
 const NavigationRouter = () => {
   return (
     <Routes>
       <Route element={<Home />} path="/" />
       <Route element={<Login />} path="login" />
       <Route element={<Register />} path="register" />
+      <Route element={<AdminDashboard />} path="admin/dashboard" />
       <Route element={<Worker />} path="worker/dashboard" />
       <Route element={<Manager />} path="manager/dashboard" />
-      <Route element={<ManagerMessage />} path="managermessage" />
-      <Route element={<WorkerMessage />} path="workermessage" />
-      <Route element={<FileUploader />} path="fileuploader" />
-      <Route element={<AdminDashboard />} path="admin/dashboard" />
-      <Route element={<RegisterUser />} path="register/user" />
-      <Route element={<ViewUser />} path="view/user" />
+      <Route element={<ManagerMessage />} path="manager/message" />
+      <Route element={<WorkerMessage />} path="worker/message" />
+      <Route element={<FileUploader />} path="file-uploader" />
+      <Route element={<Register />} path="admin/register" />
+      <Route element={<ViewUser />} path="admin/view" />
     </Routes>
   );
 };
