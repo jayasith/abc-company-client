b9024e08c512f2a6312dce6b4fbd383422adb428
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-11-11 00:45:35 +0530

    UPDATE:  refactoring

--- src/pages/Worker.jsx
+++ src/pages/Worker.jsx
@@ -1,16 +1,13 @@
-import React from "react";
-import Header from "../components/Header"; 
-import WorkerHome from "./WorkerHome";
+import React from 'react';
+import Header from '../components/Header';
+import WorkerHome from './WorkerHome';
 const Worker = () => {
   return (
     <div>
-<Header/>
-<WorkerHome/>
+      <Header />
+      <WorkerHome />
     </div>
-
   );
 };
 
 export default Worker;
-
-
