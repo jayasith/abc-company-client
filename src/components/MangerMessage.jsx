08b95f7bbe976e5e1ff71f3ae3fb504221249bf0
OnellaNatalie <onellanatalie824@gmail.com>
2022-11-11 10:20:11 +0530

    ADD : Toast Message

--- src/components/MangerMessage.jsx
+++ src/components/MangerMessage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import axios from 'axios';
-
+import { ToastContainer, toast } from "react-toastify";
 const ManagerMessage = () => {
   const [message, setMessage] = useState('');
   const [error, setError] = useState('');
@@ -20,7 +20,16 @@ const ManagerMessage = () => {
             token: token,
           },
         }
-      );
+      );   toast.success("Successfully Save Message!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
       setButtonStatus(false);
       setMessage('');
     } catch (err) {
@@ -36,7 +45,10 @@ const ManagerMessage = () => {
           {error && <p className="text-red-500 text-sm text-center">{error}</p>}
           <form>
             <div class="mb-5">
-              <label for="message" class="mb-3 block text-base font-medium text-[#07074D]">
+              <label
+                for="message"
+                class="mb-3 block text-base font-medium text-[#07074D]"
+              >
                 Message
               </label>
               <textarea
@@ -52,17 +64,32 @@ const ManagerMessage = () => {
               ></textarea>
             </div>
             <div>
-              <button
-                class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
-                onClick={saveMessage}
-                disabled={buttonStatus}
-              >
-                {buttonStatus ? 'Saving...' : 'Save'}
+              <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
+                <div class="absolute inset-0 w-3 bg-[#6A64F1] transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+                <span
+                  class="relative text-black group-hover:text-white"
+                  onClick={saveMessage}
+                  disabled={buttonStatus}
+                >
+                  {buttonStatus ? "Saving..." : "Save"}
+                </span>
               </button>
             </div>
           </form>
         </div>
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 };
