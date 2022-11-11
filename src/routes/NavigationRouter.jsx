2247ecc40c469cb794b48bd3d8519f90f83040b9
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-11-12 00:04:26 +0530

    CREATE: protected routes redirect added

--- src/routes/NavigationRouter.jsx
+++ src/routes/NavigationRouter.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 
 import Home from '../pages/Home';
 import Login from '../pages/Login';
@@ -14,18 +15,57 @@ import ViewUsers from '../pages/ViewUsers';
 import NotFound from '../pages/NotFound';
 
 const NavigationRouter = () => {
+  const [loggedIn, setLoggedIn] = useState({ state: false, role: '' });
+
+  const getLoggedIn = async () => {
+    try {
+      const res = await axios.get('/user/logged', { headers: { token: localStorage.getItem('token') } });
+      setLoggedIn({ state: res.data.state, role: res.data.role });
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getLoggedIn();
+  }, []);
+
   return (
     <Routes>
       <Route element={<Home />} path="/" />
       <Route element={<Login />} path="login" />
-      <Route element={<Register />} path="admin/register" />
-      <Route element={<AdminDashboard />} path="admin/dashboard" />
-      <Route element={<Worker />} path="worker/dashboard" />
-      <Route element={<Manager />} path="manager/dashboard" />
-      <Route element={<ManagerMessage />} path="manager/message" />
-      <Route element={<WorkerMessage />} path="worker/message" />
-      <Route element={<FileUploader />} path="file-uploader" />
-      <Route element={<ViewUsers />} path="admin/users" />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'admin' ? <Register /> : <Navigate to="/" replace />}
+        path="admin/register"
+      />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'admin' ? <AdminDashboard /> : <Navigate to="/" replace />}
+        path="admin/dashboard"
+      />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'worker' ? <Worker /> : <Navigate to="/" replace />}
+        path="worker/dashboard"
+      />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'manager' ? <Manager /> : <Navigate to="/" replace />}
+        path="manager/dashboard"
+      />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'manager' ? <ManagerMessage /> : <Navigate to="/" replace />}
+        path="manager/message"
+      />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'worker' ? <WorkerMessage /> : <Navigate to="/" replace />}
+        path="worker/message"
+      />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'manager' ? <FileUploader /> : <Navigate to="/" replace />}
+        path="file-uploader"
+      />
+      <Route
+        element={loggedIn.state && loggedIn.role === 'admin' ? <ViewUsers /> : <Navigate to="/" replace />}
+        path="admin/users"
+      />
       <Route element={<NotFound />} path="*" />
     </Routes>
   );
